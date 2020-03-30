ea4d59f98835bbd436576ec39b27097dd640be3c
Alexander Classen <ac@westwerk.ac>
2020-03-30 11:44:26 +0200

    add footer

---
+++ components/VFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <nuxt-link to="thanks" class="link">Danke</nuxt-link>
+    <nuxt-link to="support" class="link">Unterstützen</nuxt-link>
+    <a href="https://telefonkneipe.de/" target="_blank" class="link"
+      >Telefonkneipe</a
+    >
+  </footer>
+</template>
+<script>
+export default {}
+</script>
+<style></style>
