86d3d5b965401435660416553472f62fcc58b20f
Alexander Classen <ac@westwerk.ac>
2020-04-01 15:55:46 +0200

    fix footer link referer

--- components/VFooter.vue
+++ components/VFooter.vue
@@ -2,7 +2,11 @@
   <footer>
     <nuxt-link to="thanks" class="link">Danke</nuxt-link
     ><nuxt-link to="support" class="link">Unterstützen</nuxt-link
-    ><a href="https://telefonkneipe.de/" target="_blank" class="link"
+    ><a
+      rel="noreferrer"
+      href="https://telefonkneipe.de/"
+      target="_blank"
+      class="link"
       >Telefonkneipe</a
     >
   </footer>
